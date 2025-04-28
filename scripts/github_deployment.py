731a3da3d51d42020ca933ec036dd7a9a1ebfe11
ianuriegas <75506938+ianuriegas@users.noreply.github.com>
2025-04-27 19:37:51 -0500

    feat: Update deploy prd to match deploy preview, included environment as arg

--- scripts/github_deployment.py
+++ scripts/github_deployment.py
@@ -3,14 +3,14 @@ import os
 import sys
 from github import Github
 
-def update_deployment_status(token, repo_name, deployment_id, state, environment_url=None):
+def update_deployment_status(token, repo_name, deployment_id, state, environment, environment_url=None):
     g = Github(token)
     repo = g.get_repo(repo_name)
     deployment = repo.get_deployment(deployment_id)
     
     status = {
         'state': state,
-        'environment': 'Preview'
+        'environment': environment
     }
     
     if environment_url:
@@ -19,14 +19,15 @@ def update_deployment_status(token, repo_name, deployment_id, state, environment
     deployment.create_status(**status)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print("Usage: python deploy.py <token> <repo_name> <deployment_id> <state> [environment_url]")
+    if len(sys.argv) < 5:
+        print("Usage: python github_deployment.py <token> <repo_name> <deployment_id> <state> <environment> [environment_url]")
         sys.exit(1)
         
     token = sys.argv[1]
     repo_name = sys.argv[2]
     deployment_id = int(sys.argv[3])
     state = sys.argv[4]
-    environment_url = sys.argv[5] if len(sys.argv) > 5 else None
+    environment = sys.argv[5]
+    environment_url = sys.argv[6] if len(sys.argv) > 6 else None
     
-    update_deployment_status(token, repo_name, deployment_id, state, environment_url) 
+    update_deployment_status(token, repo_name, deployment_id, state, environment, environment_url)
